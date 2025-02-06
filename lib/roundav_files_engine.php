4b51e6f8e08232d4bc654af126606fc1ab834f73
nicofrand <mail@nicofrand.eu>
2025-02-06 13:58:41 +0100

    fix: add attachment

--- lib/roundav_files_engine.php
+++ lib/roundav_files_engine.php
@@ -949,7 +949,7 @@ class roundav_files_engine
                     'html'      => $content,
                     'name'      => $attachment['name'],
                     'mimetype'  => $attachment['mimetype'],
-                    'classname' => rcmail_filetype2classname($attachment['mimetype'], $attachment['name']),
+                    'classname' => rcube_utils::file2class($attachment['mimetype'], $attachment['name']),
                     'complete'  => true), $uploadid);
             }
             else if ($attachment['error']) {
